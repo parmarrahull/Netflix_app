17b53e62e90ff558437cb1ebb1a19f0b43e89152
Parmar Rahul <parmarrahul1974@gmail.com>
2021-10-09 14:59:48 +0530

    all application data added

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Cards from './Cards';
+import Sdata from './Sdata';
+
+
+const App = () => ( 
+  <>
+    <h1 className="heading_style"> List of top 6 Netflix Series in 2020 </h1>
+    {Sdata.map((val, index) => {
+      return(
+        <Cards
+          key={val.id} 
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+      );
+    })}
+  </>
+);
+
+
+export default App;
---
+++ src/Sdata.jsx
@@ -0,0 +1,51 @@
+const Sdata = [
+    {
+        id:1, 
+        imgsrc:"https://i.pinimg.com/236x/e7/1d/26/e71d2630fc1961519aa34f93d1c83775.jpg",
+        title:" Netflix Orignal Series",
+        sname: "DARK",
+        link:"https://www.netflix.com/in/title/80100172",
+    },
+
+    {
+        id:2,
+        imgsrc:"https://www.kodhit.me/wp-content/uploads/2020/05/Extracurricular-2020-%E0%B8%8A%E0%B8%A1%E0%B8%A3%E0%B8%A1%E0%B8%A5%E0%B8%B1%E0%B8%9A-%E0%B8%98%E0%B8%B8%E0%B8%A3%E0%B8%81%E0%B8%B4%E0%B8%88%E0%B8%A3%E0%B8%B1%E0%B8%81-214x300.jpg",
+        title:" Netflix Orignal Series",
+        sname: "Extra Curricular",
+        link:"https://www.netflix.com/in/title/80990668?source:35",
+    },
+
+    {
+        id:3,
+        imgsrc:"https://3.bp.blogspot.com/-U81k_h2L8pQ/WcW94W_ihuI/AAAAAAAAB1g/nXFjWZefe4QmdPcdt6zg5CggdJmONbu-ACLcBGAs/s320/strangerthings.jpg",
+        title:" Netflix Orignal Series",
+        sname: "Stranger Things",
+        link:"https://www.netflix.com/in/title/80057281",
+    },
+
+    {
+        id:4,
+        imgsrc:"https://1.bp.blogspot.com/-UG2ACZ131DU/YCAvLBVINeI/AAAAAAAAAtc/iEXwBRuV7fkS5ZYHa9reVabdzA9TOViEQCLcBGAsYHQ/s320/b682af9050b0780927fbec422914ed56.jpg",
+        title:"Amazon Orignal Series",
+        sname: "The Vampire Diaries",
+        link:"https://www.primevideo.com/detail/The-Vampire-Diaries/0HG2QOPONEONZPP2ZXNMA6M8Q7", 
+    },
+
+    {
+        id:5,
+        imgsrc:"https://muvi.cc/uploads/posts/2020-10/1602053064-1382256088.jpg",
+        title:" Netflix Orignal Series",
+        sname: "My First-2 Love",
+        link:"https://www.netflix.com/in/title/81026700", 
+    },
+
+    {
+        id:6,
+        imgsrc:"https://img.rnudah.com/grids/66/668014040065955.jpg",
+        title:" Netflix Orignal Series",
+        sname: "Money Heist",
+        link:"https://www.netflix.com/in/title/80192098", 
+    },
+];
+
+export default Sdata; 
